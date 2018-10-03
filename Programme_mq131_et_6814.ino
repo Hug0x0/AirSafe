9b2d2ae684bc39519912fb4c5a9608c7c0f5a4e7
Hugooboutot <43379546+Hugooboutot@users.noreply.github.com>
2018-10-03 17:07:05 +0200

    Add files via upload
    Arduino Capteurs de Gaz mq131 et mq6814

---
+++ Programme_mq131_et_6814.ino
@@ -0,0 +1,30 @@
+
+void setup() {
+  Serial.begin(9600);
+  
+ 
+}
+
+void loop() {
+ float Ozone = 0;
+ float NO2 = 0;
+ float NH3 = 0;
+ float CO =0;  
+  Ozone = analogRead(A0);
+  NO2 = analogRead(A1);
+  NH3 = analogRead(A2);
+  CO = analogRead(A3);
+  Serial.println("ozone:");
+  Serial.println(Ozone,DEC);
+  Serial.println("NO2:");
+  Serial.println(NO2);
+  Serial.println("NH3");
+  Serial.println(NH3);
+  Serial.println("CO:");
+  Serial.println(CO);
+   Serial.println("----------------");
+  delay(3000);
+  
+  
+
+}
